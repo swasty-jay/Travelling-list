eb7b54de06831a639d230b09cbea1032f2e235a4
swasty jay <amekpoagbedaniel@gmail.com>
2024-10-22 13:37:46 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Forms />
+      <ParkingList />
+      <Starts />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝 Far Away 💼</h1>;
+}
+
+function Forms() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function ParkingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
+function Starts() {
+  return (
+    <footer className="stats">
+      {" "}
+      👜you have X items on your list and you already parked X(X%)
+    </footer>
+  );
+}

1aa99385658b6189c5f7e0696ba2cbe4f5b36f45
swasty jay <amekpoagbedaniel@gmail.com>
2024-11-29 21:35:44 +0000

    added the form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Phone", quantity: 1, packed: true },
+  { id: 3, description: "Phone", quantity: 1, packed: true },
 ];
 
 export default function app() {
@@ -38,7 +38,7 @@ const PackinfList = () => {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>

a8116fbcec1551f6f865efa82dca94a102b53c32
swasty jay <amekpoagbedaniel@gmail.com>
2024-11-29 22:30:20 +0000

    adding form functionalities

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -18,17 +20,29 @@ const Logo = () => {
   return <h1>🌴Far Away💼</h1>;
 };
 const Form = () => {
+  const [description, setDescription] = useState("");
+
+  const [Quantity, setQuantity] = useState(1);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your😍 trip?</h3>
-      <select>
+      <select value={Quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." />
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />{" "}
+      {/* this is a controlled component */}
       <button>Add</button>
     </form>
   );
